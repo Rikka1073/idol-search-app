0a5de3b23a10ab3901aa4fd85dedb8e9a5fe257a
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-11 19:48:42 +0900

    取得するデータの数制限

--- src/utils/supabaseFunction.ts
+++ src/utils/supabaseFunction.ts
@@ -1,7 +1,12 @@
 import { supabase } from "./supabase";
 
-export const getAllData = async () => {
-  const { data, error } = await supabase.from("idol-data").select("*");
+export const getAllData = async (page: number, limit: number) => {
+  console.log(page, limit);
+  const start = (page - 1) * limit;
+  const end = start + limit - 1;
+  console.log(start, end);
+  const { data, error } = await supabase.from("idol-data").select("*").range(start, end);
+
   if (error) {
     throw new Error(error.message);
   }
