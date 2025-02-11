62aaaaf78831ef7ae00bd6c41533bd4c07c9f633
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-11 13:54:36 +0900

    データの内容表示

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,29 @@ import "./App.css";
 import { getAllData } from "./utils/supabaseFunction";
 
 function App() {
-  const { data, error, isLoading } = useSWR("fetchData", getAllData);
+  const { data, error, isLoading } = useSWR("fetchData", getAllData, { revalidateIfStale: false });
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <button className="btn btn-outline">Default</button>
+      <div>
+        {data ? (
+          <div>
+            {data.map((data) => {
+              return (
+                <div key={data.id}>
+                  <div>{data.name}</div>
+                  <div>{data.group}</div>
+                  <div>{data.country}</div>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div>データがありません</div>
+        )}
+      </div>
     </>
   );
 }
