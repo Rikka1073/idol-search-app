3cc0a181147ee5d4afa30fbf5d07538b1375a146
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-11 21:50:15 +0900

    レイアウト修正

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const Card = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="card bg-base-100 w-72 shadow-xl rounded-xl mt-10">
+    <div className="card bg-base-100 w-72 shadow-xl rounded-xl mt-10 border-1 border-gray-100">
       <div className="card-body">{children}</div>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const Header = () => {
   return (
     <header className="max-w-xl mx-auto mb-10">
-      <div className="navbar bg-base-100 rounded-xl">
-        <p className="btn btn-ghost text-xl rounded-xl">daisyUI</p>
+      <div className="navbar bg-base-100 rounded-xl border-2 border-green-300">
+        <p className="btn btn-ghost text-xl rounded-xl text-green-300">daisyUI</p>
       </div>
     </header>
   );
