480c5388aa240396244d3c28f46b96f8cfeadbec
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-11 21:49:56 +0900

    データの表示の機能実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import useSWR from "swr";
 import "./App.css";
 import { getAllData } from "./utils/supabaseFunction";
 import Header from "./components/Header";
 import Layout from "./components/Layout";
 import Card from "./components/Card";
+import { useEffect, useState } from "react";
 
 function App() {
-  const { data, error, isLoading } = useSWR("fetchData", getAllData, { revalidateIfStale: false });
+  const limit = 8;
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState<IdolData[]>([]);
+
+  type IdolData = {
+    id: number;
+    name: string;
+    group: string;
+    country: string;
+    birthday: string;
+    img: string;
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getAllData(page, limit);
+      console.log(res);
+      setData(res);
+    };
+    fetchData();
+  }, [page]);
+
+  const oncClickNext = () => {
+    setPage(page + 1);
+  };
+
+  const oncClickPrev = () => {
+    setPage(page - 1);
+  };
 
   return (
     <>
       <Layout>
         <Header />
-        <div>
-          {data ? (
+        <div className="mb-10">
+          {data && (
             <div className="flex flex-wrap justify-between">
               {data.map((data) => {
                 return (
                   <Card key={data.id}>
                     <div className="w-[70%] mx-auto h-[230px]">
-                      <img src={`${data.img}`} />
+                      <img src={`${data.img}`} className="w-full h-full" />
                     </div>
                     <div className="font-bold">{data.name}</div>
                     <div className="font-bold">{data.group}</div>
@@ -29,11 +57,30 @@ function App() {
                 );
               })}
             </div>
-          ) : (
+          )}
+          {data.length === 0 && (
             <div className="flex justify-center">
-              <span className="loading loading-spinner loading-lg"></span>
+              <div className="text-2xl font-bold">No data</div>
             </div>
           )}
+        </div>
+        <div className="flex justify-center">
+          <div className="join grid grid-cols-2">
+            <button
+              onClick={oncClickPrev}
+              className="join-item btn border-green-300 bg-white rounded-tl-xl rounded-bl-xl"
+              disabled={page === 1}
+            >
+              Previous page
+            </button>
+            <button
+              onClick={oncClickNext}
+              className="join-item btn border-green-300 bg-white rounded-tr-xl rounded-br-xl"
+              disabled={data.length < limit}
+            >
+              Next
+            </button>
+          </div>
         </div>
       </Layout>
     </>
