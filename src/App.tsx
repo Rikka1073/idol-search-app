21ae84af0a3f7587ee763c1bb2a89715d5956534
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-11 14:30:04 +0900

    headerコンポーネント作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import useSWR from "swr";
 import "./App.css";
 import { getAllData } from "./utils/supabaseFunction";
+import Header from "./components/Header";
 
 function App() {
   const { data, error, isLoading } = useSWR("fetchData", getAllData, { revalidateIfStale: false });
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button className="btn btn-outline">Default</button>
+    <div className="bg-amber-500">
+      <Header />
       <div>
         {data ? (
           <div>
@@ -26,7 +26,7 @@ function App() {
           <div>データがありません</div>
         )}
       </div>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="max-w-xl mx-auto">
+      <div className="navbar bg-base-100 rounded-xl">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
