e295009ae77aef7373606d346bf951adf2351d7b
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-11 13:35:44 +0900

    swrのインストール

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import useSWR from "swr";
 import "./App.css";
+import { getAllData } from "./utils/supabaseFunction";
 
 function App() {
+  const { data, error, isLoading } = useSWR("fetchData", getAllData);
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
