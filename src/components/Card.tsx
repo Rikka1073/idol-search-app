cf59417f7ecd79dc717305fb63ce184e33aa4dfe
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-11 15:33:20 +0900

    レイアウト修正

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const Card = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="card bg-base-100 w-96 shadow-xl">
+    <div className="card bg-base-100 w-80 shadow-xl rounded-xl">
       <div className="card-body">{children}</div>
     </div>
   );
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
-  return <div className="bg-amber-400 px-20 py-10">{children}</div>;
+  return <div className="bg-amber-400 px-20 py-10 h-screen">{children}</div>;
 };
 
 export default Layout;
