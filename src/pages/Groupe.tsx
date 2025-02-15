abe77b17d9b8ccbf7c7c0b31006df36a79faddd4
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-15 18:48:23 +0900

    Groupeページ作成

--- src/pages/Groupe.tsx
+++ src/pages/Groupe.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { getAllData } from "../utils/supabaseFunction";
+import Card from "../components/Card";
 
+type IdolData = {
+  id: number;
+  name: string;
+  group: string;
+  country: string;
+  birthday: string;
+  img: string;
+};
 const Groupe = () => {
-  return <div>おはよう</div>;
+  const { id } = useParams();
+  const [displayData, setDisplayData] = useState<IdolData[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getAllData();
+      console.log(res);
+      const filterRes = res.filter((data) => data.group === id);
+      console.log(filterRes);
+      setDisplayData(filterRes);
+    };
+    fetchData();
+  }, [id]);
+
+  return (
+    <>
+      <div className="flex flex-wrap justify-between">
+        {displayData
+          ? displayData.map((data) => {
+              return (
+                <Card key={data.id}>
+                  <div className="w-[70%] mx-auto h-[230px]">
+                    <img src={`${data.img}`} className="w-full h-full" />
+                  </div>
+                  <div className="font-bold">{data.name}</div>
+                  <div className="font-bold">{data.group}</div>
+                  <div className="font-bold">{data.country}</div>
+                  <div className="font-bold">{data.birthday}</div>
+                </Card>
+              );
+            })
+          : null}
+      </div>
+    </>
+  );
 };
 
 export default Groupe;
