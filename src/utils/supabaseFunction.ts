ef3dcbe5d76af35019767816e9506c43cad86ddb
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-15 18:30:27 +0900

    全アイドルのデータを取得する関数の作成

--- src/utils/supabaseFunction.ts
+++ src/utils/supabaseFunction.ts
@@ -1,11 +1,21 @@
 import { supabase } from "./supabase";
 
-export const getAllData = async (page: number, limit: number) => {
+export const getIdolData = async (page: number, limit: number) => {
   console.log(page, limit);
   const start = (page - 1) * limit;
   const end = start + limit - 1;
   console.log(start, end);
   const { data, error } = await supabase.from("idol-data").select("*").range(start, end);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+  console.log(data);
+  return data;
+};
+
+export const getAllData = async () => {
+  const { data, error } = await supabase.from("idol-data").select("*");
 
   if (error) {
     throw new Error(error.message);
