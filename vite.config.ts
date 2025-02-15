8fbc550cf0bcd514ede3ae53b4abc123f4741160
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-15 18:44:35 +0900

    baseのURL指定

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,5 @@ import env from "vite-plugin-env-compatible";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), env({ prefix: "VITE", mountedPath: "process.env" })],
+  base: "/Idol",
 });
