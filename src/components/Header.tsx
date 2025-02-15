72eb0ea6b79b1aa7acf125b754873e605c584bc4
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-15 12:41:45 +0900

    header UI変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,16 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="max-w-xl mx-auto mb-10">
-      <div className="navbar bg-base-100 rounded-xl border-2 border-green-300">
+    <header className="w-full mb-10">
+      <div className="navbar bg-base-100  border-b-2 border-green-300 justify-between">
         <p className="btn btn-ghost text-xl rounded-xl text-green-300">daisyUI</p>
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered focus:outline-none w-md rounded-xl"
+          />
+        </div>
       </div>
     </header>
   );
