d3ddfc0a46e180942249e424c51ca8e055274ef8
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-15 18:45:53 +0900

    検索欄削除

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,19 @@
-import React from "react";
-
 const Header = () => {
   return (
     <header className="w-full mb-10">
       <div className="navbar bg-base-100  border-b-2 border-green-300 justify-between">
-        <p className="btn btn-ghost text-xl rounded-xl text-green-300">daisyUI</p>
-        <div className="form-control">
+        <a href="/Idol" className="btn btn-ghost text-xl rounded-xl text-green-300">
+          daisyUI
+        </a>
+        {/* <div className="form-control">
           <input
             type="text"
             placeholder="Search"
             className="input input-bordered focus:outline-none w-md rounded-xl"
+            value={inputValue}
+            onChange={handleInputChange}
           />
-        </div>
+        </div> */}
       </div>
     </header>
   );
